c0cb20a5251e3ad48c3fa5a2d489c1353fce6e2e
Josh Koontz <24576918+joshk326@users.noreply.github.com>
2023-03-21 12:46:48 -0400

    Added some demo code

--- Final Code/main.py
+++ Final Code/main.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 # import asyncio, os
-import movement, vision
+from movement import Drone, asyncio
 
 
 ''' This will me the main file ran combining both our movement and vision code '''
+async def run():
+	drone = Drone("")
+	await drone.connect()
+
+	await drone.takeoff()
+
+	await drone.left(5.0, 4)
+
+	await drone.right(15.0, 4)
+
+	await drone.up(5.0, 4)
+
+	await drone.down(5.0, 4)
+
+	await drone.land()
+
+	await drone.disconnect()
+
 
 if __name__ == '__main__':
-	drone = Drone("")
-
-	asyncio.run(drone.connect())
+	asyncio.run(run())
