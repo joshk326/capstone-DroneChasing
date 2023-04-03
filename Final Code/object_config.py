635f7d87b19871e76958956ca9ac54447071cd7a
Josh Koontz <24576918+joshk326@users.noreply.github.com>
2023-04-03 17:30:41 -0400

    Renamed from camera to object

--- Final Code/object_config.py
+++ Final Code/object_config.py
@@ -1,13 +1,12 @@
-class CameraConfig():
+class ObjectConfig():
     def __init__(self):
-        self.config_done = False
+        self.detected = False
 
     def set_config(self, initial_frame_area, coordinate_thresh, area_thresh):
+        self.detected = True
         self.initial_frame_area = initial_frame_area
         self.coordinate_thresh = coordinate_thresh
         self.area_thresh = area_thresh
 
-        self.config_done = True
-
-    def check_config_status(self):
-        return self.config_done
+    def check_detected_status(self):
+        return self.detected
