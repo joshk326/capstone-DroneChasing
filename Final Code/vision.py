1d343cfd9d531884341e414db622b230e52ac102
Josh Koontz <24576918+joshk326@users.noreply.github.com>
2023-04-10 18:09:32 -0400

    Updated vision for threading

--- Final Code/vision.py
+++ Final Code/vision.py
@@ -12,10 +12,13 @@ class Vision():
 
 	_frame_width = 500
 	_frame_height = 500
-
+	_detected_center = (0, 0)
 	_x, _y, _w, _h = 0, 0, 0, 0
+	_x_diff = 0
+	_y_diff = 0
 
 	object_detected = False
+	is_detecting = False
 
 	def __init__(self, camera):
 		self.cap = camera
@@ -34,8 +37,14 @@ class Vision():
 		y_diff = frame_center[1] - detected_center[1]
 		return (x_diff, y_diff)
 	
+	def convert_px_to_m(self, px):
+		return px * 0.0002645833
+
 	def get_object_coordinates(self):
-		return (self.x_diff, self.y_diff)
+		return self._detected_center
+	
+	def get_center_diff(self):
+		return (self._x_diff, self._y_diff)
 	
 	def get_box_dimenstions(self):
 		return [self._w, self._h]
@@ -47,42 +56,53 @@ class Vision():
 		return self.object_detected
 	
 	def detect_color(self):
-		ret, frame = self.cap.read()
+		self.is_detecting = True
 
-		font = cv2.FONT_HERSHEY_SIMPLEX
-		cv2.putText(frame, 'FPS: ' + str(self.cap.get(cv2.CAP_PROP_FPS)), (10, 50), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+		while self.is_detecting:
+			ret, frame = self.cap.read()
+			
+			font = cv2.FONT_HERSHEY_SIMPLEX
+			cv2.putText(frame, 'FPS: ' + str(self.cap.get(cv2.CAP_PROP_FPS)), (10, 50), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-		frame_center = (int(640/2), int(self._frame_height/2))
-		cv2.circle(frame, frame_center, 5, (255, 0, 0), -1)
+			frame_center = (int(640/2), int(self._frame_height/2))
+			cv2.circle(frame, frame_center, 5, (255, 0, 0), -1)
 
-		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-		mask1 = cv2.inRange(hsv, self._lower_red, self._upper_red)
-		mask2 = cv2.inRange(hsv, self._lower_red2, self._upper_red2)
-		mask = mask1 + mask2
-		cv2.imshow("Frame",mask)
-		contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-		
-		if len(contours) > 0:
-			if not self.object_detected:
-				largest_contour = max(contours, key=cv2.contourArea)
-				self._x, self._y, self._w, self._h = cv2.boundingRect(largest_contour)
-				self.object_detected = True
-			else:
-				largest_contour = max(contours, key=cv2.contourArea)
-				self._x, self._y, self._w, self._h = cv2.boundingRect(largest_contour)
+			hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+			mask1 = cv2.inRange(hsv, self._lower_red, self._upper_red)
+			mask2 = cv2.inRange(hsv, self._lower_red2, self._upper_red2)
+			mask = mask1 + mask2
+			cv2.imshow("Frame",mask)
+			contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+			
+			if len(contours) > 0:
+				if not self.object_detected:
+					largest_contour = max(contours, key=cv2.contourArea)
+					self._x, self._y, self._w, self._h = cv2.boundingRect(largest_contour)
+					self.object_detected = True
+				else:
+					largest_contour = max(contours, key=cv2.contourArea)
+					self._x, self._y, self._w, self._h = cv2.boundingRect(largest_contour)
 
-		
-		cv2.rectangle(frame, (self._x, self._y), (self._x+self._w, self._y+self._h), (0, 255, 0), 2)
-		cv2.putText(frame, "Tracked Object Cords: X- " + str(self._x) + " Y- " + str(self._y), (self._frame_width-200, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-		detected_center = (int(self._x+self._w/2), int(self._y+self._h/2))
-		cv2.circle(frame, detected_center, 5, (255, 0, 0), -1)
+			
+			cv2.rectangle(frame, (self._x, self._y), (self._x+self._w, self._y+self._h), (0, 255, 0), 2)
+			cv2.putText(frame, "Tracked Object Cords- X: " + str(self._x) + " Y: " + str(self._y), (self._frame_width-200, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+			detected_center = (int(self._x+self._w/2), int(self._y+self._h/2))
+			self._detected_center = detected_center
+			cv2.circle(frame, detected_center, 5, (255, 0, 0), -1)
 
-		x_diff, y_diff = self._center_diff(frame_center, detected_center)
-		cv2.putText(frame, "X Diff: " + str(x_diff) + " Y Diff: " + str(y_diff), (self._frame_width-200, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+			x_diff, y_diff = self._center_diff(frame_center, detected_center)
+			self._x_diff = x_diff
+			self._y_diff = y_diff
+			cv2.putText(frame, "X Diff: " + str(x_diff) + " Y Diff: " + str(y_diff), (self._frame_width-200, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-		cv2.putText(frame, "Frame Height: " + str(self._h) + " Frame Width: " + str(self._w), (self._frame_width-200, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+			cv2.putText(frame, "Frame Height: " + str(self._h) + " Frame Width: " + str(self._w), (self._frame_width-200, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-		cv2.imshow('Tracked Object', frame)
+			cv2.imshow('Tracked Object', frame)
+
+			if cv2.waitKey(25) & 0xFF == ord('q'):
+				self.is_detecting = False
+				print("-- Stopping Color Detection")
+				break
 
 	def stop_all_detection(self):
 		self.cap.release()
