2fd6df051319b27ff46537c39f0b32e7bd6410e6
Josh Koontz <24576918+joshk326@users.noreply.github.com>
2023-04-19 16:23:05 -0400

    updated config

--- Final Code/object_config.py
+++ Final Code/object_config.py
@@ -4,12 +4,18 @@ class ObjectConfig():
         self.area_thresh = 0
         self.coordinate_thresh = 0
         self.initial_frame_area = 0
-        self.intiali_box_dimensions = 0
-    def set_config(self, initial_frame_area, intail_box_dim, coordinate_thresh, area_thresh):
+        self.intial_box_dimensions = 0
+        self.intial_coords = [0,0]
+    def set_config(self, initial_frame_area, intail_box_dim, coordinate_thresh, area_thresh, intial_coords):
         self.initial_frame_area = initial_frame_area
         self.coordinate_thresh = coordinate_thresh
         self.area_thresh = area_thresh
         self.intial_box_dimensions = intail_box_dim
+        self.intial_coords = intial_coords
+    def get_intial_coords(self):
+        return self.intial_coords
+    def set_intial_coords(self, coords):
+        self.intial_coords = coords
     def check_detected_status(self):
         return self.detected
     def get_area_thresh(self):
