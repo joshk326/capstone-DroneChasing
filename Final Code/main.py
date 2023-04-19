b122c342599628b718de5fa113d584a1242e1f43
Josh Koontz <24576918+joshk326@users.noreply.github.com>
2023-04-19 16:23:52 -0400

    Updated main pipeline

--- Final Code/main.py
+++ Final Code/main.py
@@ -13,7 +13,7 @@ async def run():
 	vision = Vision(cv2.VideoCapture(0))
 	detectedObj = ObjectConfig()
 
-	coordinate_thresh = 50 # How many pixels we allow before we tell the drone to move
+	coordinate_thresh = 20 # How many pixels we allow before we tell the drone to move
 	area_thresh = 200 # How much change in rectangle area we allow before we move forward/backward
 
 	if(await drone.connect() == False):
@@ -29,7 +29,7 @@ async def run():
 		box_dim = vision.get_box_dimenstions()
 		if(vision.object_is_detected() and detectedObj.check_detected_status() == False):
 			detectedObj.update_detected_status(True)
-			detectedObj.set_config(area, box_dim, coordinate_thresh, area_thresh)
+			detectedObj.set_config(area, box_dim, coordinate_thresh, area_thresh, vision.get_object_coordinates())
 			print("\n-- Object Detected\n")
 		elif(vision.object_is_detected() and detectedObj.check_detected_status() == True):
 			#print(detectedObj.get_intial_area())
@@ -40,21 +40,25 @@ async def run():
 			diff = vision.get_center_diff()
 			move_x = vision.convert_px_to_m(abs(diff[0]))
 			move_y = vision.convert_px_to_m(abs(diff[1]))
-
-			if coordinates[0] < detectedObj.coordinate_thresh:
+			print("0:",coordinates[0], "1:",coordinates[1])
+			if coordinates[0] > detectedObj.get_intial_coords()[0] + detectedObj.coordinate_thresh:
 				await drone.left(move_x, 1.0)
-			elif coordinates[0] > -detectedObj.coordinate_thresh:
+				detectedObj.set_intial_coords([coordinates[0], coordinates[1]])
+			elif coordinates[0] < detectedObj.get_intial_coords()[0] - detectedObj.coordinate_thresh:
 				await drone.right(move_x, 1.0)
-			if coordinates[1] < -detectedObj.coordinate_thresh:
+				detectedObj.set_intial_coords([coordinates[0], coordinates[1]])
+			if coordinates[1] > detectedObj.get_intial_coords()[1] + detectedObj.coordinate_thresh:
 				await drone.down(move_y, 1.0)
-			elif coordinates[1] > detectedObj.coordinate_thresh:
+				detectedObj.set_intial_coords([coordinates[0], coordinates[1]])
+			elif coordinates[1] < detectedObj.get_intial_coords()[1] - detectedObj.coordinate_thresh:
 				await drone.up(move_y, 1.0)
+				detectedObj.set_intial_coords([coordinates[0], coordinates[1]])
 
 			frame_area = vision.get_box_area()
 
-			if(frame_area > detectedObj.initial_frame_area + detectedObj.area_thresh):
+			if(frame_area <  detectedObj.initial_frame_area + detectedObj.area_thresh):
 				await drone.backward(5.0, 4.0)
-			elif(frame_area < detectedObj.initial_frame_area - detectedObj.area_thresh):
+			elif(frame_area > detectedObj.initial_frame_area - detectedObj.area_thresh):
 				await drone.forward(5.0, 4.0)
 
 		
